cab069d543ff05f69151a946d381ce7e1b58a470
James Gregory <james@jagregory.com>
2016-03-04 17:56:14 +1100

    Bump release

--- cfval.rb
+++ cfval.rb
@@ -1,8 +1,8 @@
 class Cfval < Formula
   desc "The CloudFormation template validator"
   homepage "https://github.com/jagregory/cfval"
-  url "https://github.com/jagregory/cfval/releases/download/v0.1.0/cfval-0.1.0.tar.gz"
-  sha256 "ee27bfb203be2744039861011bd017516eabd89d6348bb18e32d33b4a8afcda2"
+  url "https://github.com/jagregory/cfval/releases/download/v0.2.0/cfval-0.2.0.tar.gz"
+  sha256 "076c6cba05047a1ce30764c9ad5369d83434d92009e016a3340beefdb898a0ef"
 
   bottle :unneeded
 
